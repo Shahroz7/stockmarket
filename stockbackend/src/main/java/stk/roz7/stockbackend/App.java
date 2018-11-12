1d29aff46039e52a526d5613050ff80ed5597de7
Shahroz7 <shahrose7@gmail.com>
2018-11-12 12:15:12 +0530

    Setup completed successfully

---
+++ stockbackend/src/main/java/stk/roz7/stockbackend/App.java
@@ -0,0 +1,13 @@
+package stk.roz7.stockbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
